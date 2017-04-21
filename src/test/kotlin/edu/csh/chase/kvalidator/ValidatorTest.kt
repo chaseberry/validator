2f08b51a4a490d32b5115f67bf491a18734424aa
Chase <chase@csh.rit.edu>
2017-04-21 10:42:49 -0400

    Added more testing cases

--- src/test/kotlin/edu/csh/chase/kvalidator/ValidatorTest.kt
+++ src/test/kotlin/edu/csh/chase/kvalidator/ValidatorTest.kt
@@ -6,15 +6,47 @@ import org.junit.Test
 
 class ValidatorTest {
 
-
     @Test
-    fun validatingBooleanTest() {
+    fun validateBooleanTest() {
         val field = required(Types.boolean, "")
 
         val result = field.checkAgainst(true)
 
         assertEquals(ValidatorStatus.OK, result.status)
+        assertEquals(Types.boolean.name, result.type)
+        assertEquals(true, result.value)
     }
 
+    @Test
+    fun invalidBooleanTest() {
+        val field = required(Types.boolean, "")
 
+        val result = field.checkAgainst(15)
+
+        assertEquals(ValidatorStatus.ERROR, result.status)
+        assertEquals(15, result.value)
+        assertEquals(Types.int.name, result.type)
+    }
+
+    @Test
+    fun validateStringTest() {
+        val field = required(Types.string, "")
+
+        val result = field.checkAgainst("Hello World!")
+
+        assertEquals(ValidatorStatus.OK, result.status)
+        assertEquals(Types.string.name, result.type)
+        assertEquals("Hello World!", result.value)
+    }
+
+    @Test
+    fun invalidStringTest() {
+        val field = required(Types.string, "")
+
+        val result = field.checkAgainst(true)
+
+        assertEquals(ValidatorStatus.ERROR, result.status)
+        assertEquals(Types.boolean.name, result.type)
+        assertEquals(true, result.value)
+    }
 }
