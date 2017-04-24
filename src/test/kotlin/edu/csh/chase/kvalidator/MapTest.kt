cc2fa062c8281cfd08fef64082cb3a47ea43b11e
Chase <chase@csh.rit.edu>
2017-04-24 09:56:40 -0400

    Added a test for basic types validation in a map

---
+++ src/test/kotlin/edu/csh/chase/kvalidator/MapTest.kt
@@ -0,0 +1,29 @@
+package edu.csh.chase.kvalidator
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class MapTest {
+
+    @Test fun validateMapTest() {
+        val map = mapOf(
+                "string" to "string",
+                "int" to 1567,
+                "double" to 15.67,
+                "boolean" to true
+        )
+
+        val result = map.validate(
+                required(Types.string, "string"),
+                required(Types.int, "int"),
+                required(Types.double, "double"),
+                required(Types.boolean, "boolean")
+        )
+
+        assertEquals(ValidatorStatus.OK, result.status)
+        assert(result.extraFields.isEmpty())
+
+    }
+
+
+}
